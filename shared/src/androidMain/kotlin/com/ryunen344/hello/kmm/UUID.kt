d9f22e66655215628437c352df16f2553b6ccb4c
RyuNen344 <s1100633@outlook.com>
2021-12-20 02:21:30 +0900

    add UUID

---
+++ shared/src/androidMain/kotlin/com/ryunen344/hello/kmm/UUID.kt
@@ -0,0 +1,5 @@
+package com.ryunen344.hello.kmm
+
+import java.util.*
+
+actual fun randomUUID() : String = UUID.randomUUID().toString()
---
+++ shared/src/commonMain/kotlin/com/ryunen344/hello/kmm/UUID.kt
@@ -0,0 +1,3 @@
+package com.ryunen344.hello.kmm
+
+expect fun randomUUID() : String
