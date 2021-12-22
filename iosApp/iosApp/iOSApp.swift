099125508084019148e4b0da1bfcf56b9d92d7e8
RyuNen344 <s1100633@outlook.com>
2021-12-23 04:57:09 +0900

    add ios implementation

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -4,9 +4,12 @@ import shared
 @main
 struct iOSApp: App {
     private let api = HttpClientEngineProviderImplKt.provideApi()
+    
+    private let database = DatabaseProviderKt.provideDatabase(sqlDriverProvider: SqlDriverProviderImpl())
+    
 	var body: some Scene {
 		WindowGroup {
-			ContentView(viewModel: .init(api: api))
+            ContentView(viewModel: .init(api: api, database: database))
 		}
 	}
 }
